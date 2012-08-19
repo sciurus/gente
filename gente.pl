d7bfd38c51ba0dd146006675c362da6b5f643c76
Brian Pitts <brian@polibyte.com>
2012-08-19 12:08:16 -0500

    Untested attempt to remove nesting

--- gente.pl
+++ gente.pl
@@ -35,52 +35,53 @@ post '/' => sub {
 
   my $error;
   my $result;
-  my $status = 200;
 
-  # these nested ifs are ridiculously hard to follow
-  # should return early instead
   my $ldap = Net::LDAP->new($server);
   if ( not $ldap ) {
     $error = "Unable to connect to $server";
     $self->app->log->error($error);
-    $status = 500;
     $result = 'An internal error occured';
+    $self->render_exception($result);
+    return;
+  }
+
+  $self->app->log->debug('LDAP Connected');
+
+  my $mesg = $ldap->start_tls( verify => 'require', cafile => $cafile );
+  if ( $mesg->code ) {
+    $error = "Unable to start TLS to $server using $cafile";
+    $self->app->log->error($error);
+    $result = 'An internal error occured';
+    $self->render_exception($result);
+    return;
+  }
+
+  $self->app->log->debug('TLS Enabled');
+
+  $mesg = $ldap->bind( "uid=$username,$dn", password => $old );
+  if ( $mesg->code ) {
+    $error = "Unable to bind as $username. Server says " . $mesg->error;
+    $self->app->log->info($error);
+    $result = 'Unable to change your password. Try again or get help.';
+    $self->render_exception($result);
+    return;
+  }
+
+  $self->app->log->debug('User Bound');
+
+  $mesg = $ldap->set_password( oldpasswd => $old, newpasswd => $new );
+  if ( $mesg->code ) {
+    $error =
+      "Unable to change password as $username. Server says " . $mesg->error;
+    $self->app->log->info($error);
+    $result = 'Unable to change your password. Try again or get help.';
   }
   else {
-    $self->app->log->debug('LDAP Connected');
-    my $mesg = $ldap->start_tls( verify => 'require', cafile => $cafile );
-    if ( $mesg->code ) {
-      $error = "Unable to start TLS to $server using $cafile";
-      $self->app->log->error($error);
-      $status = 500;
-      $result = 'An internal error occured';
-    }
-    else {
-      $self->app->log->debug('TLS Enabled');
-      $mesg = $ldap->bind( "uid=$username,$dn", password => $old );
-      if ( $mesg->code ) {
-        $error = "Unable to bind as $username. Server says " . $mesg->error;
-        $self->app->log->info($error);
-        $result = 'Unable to change your password. Try again or get help.';
-      }
-      else {
-        $self->app->log->debug('User Bound');
-        $mesg = $ldap->set_password( oldpasswd => $old, newpasswd => $new );
-        if ( $mesg->code ) {
-          $error = "Unable to change password as $username. Server says "
-            . $mesg->error;
-          $self->app->log->info($error);
-          $result = 'Unable to change your password. Try again or get help.';
-        }
-        else {
-          $self->app->log->debug('Password Changed');
-          $result = 'Your password was successfully changed';
-        }
-      }
-    }
-    $ldap->unbind();
+    $self->app->log->debug('Password Changed');
+    $result = 'Your password was successfully changed';
   }
-  $self->render( 'feedback', status => $status, result => $result );
+
+  $self->render( 'feedback', result => $result );
 
 };
 
